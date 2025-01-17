597f2d4d9c524ef30311b5480140baa87dfeef10
wxyhly <1065750837@qq.com>
2025-01-17 23:39:09 +0800

    Site updated: 2025-01-17 23:38:57

---
+++ programs/periodic4d.js
@@ -0,0 +1,165 @@
+const data1 = `					@	@																																																					@	@				
+%	%	%	%	#	H氕	D氘	@	@																																					@	@	@	@	@	@	@	@	@	@	@	@	@	@	T氚	He氦				
+%	%	%	%	#	Li锂	Lt铤	Lb钹	Be铍																																					Bc磐	B硼	Qb矻	Q磻	Qc砠	C碳	Tk𥓬	E硝	Tn磴	N氮	Ny氤	O氧	Fn氛	Fd氲	F氟	Ne氖				
+%	%	%	%	#	Na钠	Nt铙	Nm𨰾	Mg镁	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	@	Ml镙	Al铝	Da锾	Dd砃	Du砡	Si硅	Gs磪	G碓	Gp䂾	P磷	Ps硖	S硫	Lp硠	Ld㲶	Cl氯	Ar氩				
+%	%	%	%	#	K钾	Kt鉲	Cj钘	Ca钙	Sa𫔌	Sc钪	Sk镩	Hn铖	Ht镬	Ti钛	Tq镡	Ai铚	Vj𬭫	V钒	Vz𬬬	Az銸	Cz𫓥	Cr铬	Bb镑	Bn鉼	Bm䤮	Mn锰	Fx锴	Fe铁	Fw䥽	Wd釫	Cw鍢	Co钴	Ch鈨	Sp铴	Ns钑	Ni镍	Pi鎃	Pk铇	Cp𨱎	Hj锲	Hc锏	Cu铜	Hz镔	Zn锌	Gz鍜	Ga镓	Bz𫓬	Bj铡	Bg錊	Ge锗	Ih䃎	Ia砧	Iv磌	As砷	Ae䃏	Se硒	Bs𪶭	Bd溴	Br𣱕	Kr氪				
+%	%	%	%	#	Rb铷	Rt𨧨	Rs鐩	Sr锶	Sy𫓹	Y钇	Yz鈏	Z铎	Zt镃	Zr锆	Zb鉡	Zk鐰	Nk錼	Nb铌	Hb锛	Hx鑛	Mj鑂	Mo钼	Mb铫	Bo𫔆	Bt釱	Tc锝	Tr鏶	Ru钌	Rd鏴	Me鋈	Mw铉	Rh铑	Rv銋	Mu𬭁	Mp鉖	Pd钯	Ah锷	Hi铘	Ig镒	Aa鈪	Ak锵	Ag银	Ad鈄	Cd镉	Sj𬬱	In铟	Mi銤	M铓	Mq𨭩	Sn锡	Ze䤠	Ci鈶	Cg𨥜	Sb锑	St碁	Te碲	It碤	Id砥	I碘	Xe氙				
+%	%	%	%	#	Cs铯	Ct铩	Cb鎟	Ba钡	Bu鑗	Lu镥	Uv䤸	Hv铔	Vf鍝	Hf铪	Uf鍑	Uk锳	Ut䥙	Ta钽	Tt鎕	Mh釯	Wm釪	W钨	Wo鉥	Ot锬	Or䥃	Re铼	Ro鍒	Os锇	Zo𫟻	Zi鐑	My𬭆	Ir铱	El鏂	Ao䥝	So𫓪	Pt铂	Lh鏸	Fa𫔍	Fu鍸	J𨱓	Aj𫓦	Au金	Jg釶	Hg汞	Tg釭	Tl铊	Ql钶	Qm钤	Pq銙	Pb铅	Pl鐱	Ap镛	By𫔇	Bi铋	Bp鈵	Po钋	Pe碚	Et砈	At砹	Rn氡				
+%	%	%	%	#	Fr钫	Ft鍅	Fc钣	Ra镭	Rr釢	Lr铹	Lc𫓺	Pc鉘	Pf鉜	Rf𬬻	Rk顉	Lk铆	Jd鎌	Db𬭊	Dh鍕	Eh銰	Eg𬬧	Sg𬭳	Pg铸	Pn鍻	Ph𬭖	Bh𬭛	Bw䥤	Hs𬭶	Hm𫟺	Hu锪	Ez鉙	Mt鿏	Ov鉒	Ve䥳	Do鍎	Ds𫟼	Zu𬬺	Zh鉊	Zx鏉	Ck鏪	Gk𬬪	Rg𬬭	Kn銶	Cn鿔	Nc鈤	Nh鿭	Sh鍉	Su鋜	Fs鏠	Fl𫓧	Fb𬬯	Ab鎴	Aq铰	Mc镆	Mv鍡	Lv𫟷	Ls鈻	Td𬭓	Ts钿	Og鿫				
+																																																																
+#	Ln鋓	La镧	Lz釼	L釖	Le釰	Ce铈	Kc鏮	Ka𫔋	Kp鏿	Pr镨	Fp錗	Mf鍲	Km钎	Dk镗	Dt𬭬	Nd钕	Zd钊	Tz铳	Hw鈋	Gt鈊	Gm釛	Pm钷	Mx铐	Xd鏬	Xc鏭	Sm钐	Ji鎅	Tj钝	Js鈌	Eu铕	Gu锢	Ee𫔅	Kl镂	Kk锚	Gn鈝	Gd钆	Gj鑇	Rj𨱕	Jt䦂	Tb铽	Kb錓	Mk鍷	Kd鐁	Dy镝	Df鑆	Sf鉎	Fh鏣	Sw鏁	Wj钜	Ho钬	Vo锸	Cv𫓩	Vd鍨	Hd䤣	Hr锽	Er铒	Xk𫓴	Aw铻	Jw鏩	Tm铥	Jm鏋	Ri鏫	Cy锖	Yb镱
+#	An錌	Ac锕	Ma镘	Ju鈢	Jh銲	Th钍	Vh𬭃	Kh鑘	Kj𬭿	Pa镤	Sq𨱑	Sz𫟳	Sv𫔊	Ty钺	Tu鏕	U铀	Ur𫔉	Mr𨱔	Mz镞	A𨤿	Ay𫒉	Np镎	Tp𨱏	To镋	Tw鋔	Pu钚	Pv鐻	Ey𬬩	Em铧	Am镅	Rm鎠	Ry镮	Rw镟	En钲	Cq锜	Cm锔	Je镊	Ej镢	Jk𨱄	Bk锫	Bl𫔒	Bf镚	Bq镵	Cf锎	Rc鉌	Rp锟	Py𫔐	Mm𫓯	Vm铏	Es锿	Ss䦅	Sl𫔕	Sd𨰿	De𨱐	Dm釥	Fm镄	Af𨥁	Ax鎐	Xm鎋	Md钔	Vp釮	Vc锒	Vn镏	No锘
+`;
+const tb = document.createElement("table");
+const alt = "1s,2s,2p,3s,3p,4s,3d,4p,5s,4d,5p,6s,4f,5d,6p,7s,5f,6d,7p,8s".split(',');
+const size = { s: 4, p: 16, d: 36, f: 64 };
+const nonMetal = "1,2,3,9-23,32-43,91-99,150-155,273-275".split(',');
+const Hepleto = "4,24,44,100,156,276,396".split(',').map(e => parseInt(e));
+const modifiedTable = Object.fromEntries(`
+    55:3d9 4s2
+    65:3d18 4s3
+    64:3d18 4s2
+    63:3d18 4s1
+    74:3d27 4s3
+    73:3d27 4s2
+    81:3d36 4s1
+    82:3d36 4s2
+    83:3d36 4s3
+   110:4d9 5s1
+   111:4d9 5s2
+   112:4d9 5s3
+   118:4d16 5s2
+   119:4d18 5s1
+   120:4d18 5s2
+   121:4d18 5s3
+   129:4d27 5s2
+   130:4d27 5s3
+   137:4d36 5s1
+   138:4d36 5s2
+   139:4d36 5s3
+   240:5d18 6s2
+   241:5d18 6s3
+   249:5d27 6s2
+   256:5d34 6s2
+   257:5d36 6s1
+   258:5d36 6s2
+   259:5d36 6s3
+   377:6d36 7s1
+   378:6d36 7s2
+   379:6d36 7s3
+   161:4f0 5d1
+   162:4f0 5d2
+   163:4f1 5d2
+   164:4f2 5d2
+   165:4f4 5d1
+   177:4f16 5d1
+   178:4f16 5d2
+   193:4f32 5d1
+   194:4f32 5d2
+   209:4f48 5d1
+   281:5f0 6d1
+   282:5f0 6d2
+   283:5f0 6d3
+   284:5f0 6d4
+   285:5f3 6d2
+   286:5f4 6d2
+   297:5f16 6d1
+   313:5f32 6d1
+   314:5f32 6d2
+   329:5f48 6d1
+`.trim().split("\n").map(e => {
+    const [n, v] = e.split(":");
+    return [n.trim(), v.split(" ")];
+}));
+let electrons = [];
+let electronsText = [];
+tb.classList.add("periodic");
+// whole table
+document.getElementById("periodic")?.appendChild(tb);
+
+// only H D T He
+const HDTHe = (document.getElementById("periodic-1st")) ? true : false;
+document.getElementById("periodic-1st")?.appendChild(tb);
+// calculate electrons
+for (let id_num = 1; id_num <= 396; id_num++) {
+    if (electrons.length) {
+        const q = size[alt[electrons.length - 1][1]];
+        if (electrons[electrons.length - 1] === q) {
+            electrons.push(1);
+        } else {
+            electrons[electrons.length - 1]++;
+        }
+    } else {
+        electrons.push(1);
+    }
+    let modified = electrons;
+
+    if (modifiedTable[id_num]) {
+        modified = electrons.slice(0);
+        const newVal = modifiedTable[id_num];
+        for (const v of newVal) {
+            modified[alt.indexOf(v.slice(0, 2))] = parseInt(v.slice(2));
+        }
+    }
+    electronsText.push(modified.map((v, i) => {
+        if (!v || i < [null, 0, 1, 1, 3, 3, 5, 5, 5, 8, 8, 8, 11, 11, 11, 11, 15, 15, 15, 15, 15][electrons.length]) return "";
+        if ({ 8: [6], 11: [9], 14: [12], 15: [12, 13], 18: [16], 19: [16, 17] }[electrons.length]?.includes(i)) return "";
+        return alt[i] + `<sup${modified === electrons ? "" : " class='modified'"}>${v}</sup>`;
+    }).filter(e => e).sort((a, b) => a[0] - b[0]));
+}
+let id_num = 1;
+let cols = 0;
+let rows = 0;
+
+if (HDTHe) {
+    tb.style.width = "100px";
+    for (const line of "H氕\tD氘\tT氚\tHe氦".split("\n").map(e => e.split("\t"))) {
+        const tr = document.createElement("tr");
+        tb.appendChild(tr);
+        for (const elem of line) {
+            const td = document.createElement("td");
+            tr.appendChild(td);
+            td.classList.add(id_num===4?"Hepleto":"nonmetal");
+            if (elem.length === 1 || !elem.trim()) { td.innerHTML = "<br/>"; td.classList.add("empty"); continue; }
+            
+            const textLen = electronsText[id_num - 1].length;
+            td.innerHTML = `<div class='id-num'>${id_num}</div><span class="elem-name">${elem}</span><div class='electron ${textLen > 3 ? "smaller" : textLen > 2 ? "small" : ""}'>${electronsText[id_num - 1].join("")}</div>`;
+            id_num++;
+        }
+    }
+} else {
+    for (const line of data1.split("\n").map(e => e.split("\t"))) {
+        const tr = document.createElement("tr");
+        tb.appendChild(tr);
+        for (const elem of line) {
+            const td = document.createElement("td");
+            tr.appendChild(td);
+            if (elem === "#") { td.innerHTML = "1,2,3,4,5,6,7,镧系,锕系".split(",")[rows++]; td.classList.add("rows"); continue; }
+            if (elem === "@") { td.innerHTML = ["IA", "IIA", "XIXA", "XXA", "IIIA", "IVA", "VA", "VIA", "VIIA", "VIIIA", "IXA", "XA", "XIA", "XIIA", "XIIIA", "XIVA", "XVA", "XVIA", "XVIIA", "XVIIIA", "VB", "VIB", "VIIB", "VIIIB", "IXB", "XB", "XIB", "XIIB", "XIIIB", "XIVB", "XVB", "XVIB", "XVIIB", "XVIIIB", "XIXB", "XXB", "XXIB", "XXIIB", "XXIIIB", "XXIVB", "XXVB", "XXVIB", "XXVIIB", "XXVIIIB", "XXIXB", "XXXB", "XXXIB", "XXXIIB", "XXXIIIB", "XXXIVB", "XXXVB", "XXXVIB", "IB", "IIB", "IIIB", "IVB"][cols++]; td.classList.add("cols"); continue; }
+            if (elem.length === 1 || !elem.trim()) { td.innerHTML = "<br/>"; td.classList.add("empty"); continue; }
+            if (id_num === 225 || id_num === 345) { td.classList.add("la-ac-fini"); }
+            else if (rows >= 8) td.classList.add("la-ac");
+            else if (Hepleto.includes(id_num)) td.classList.add("Hepleto");
+            else {
+                for (const v of nonMetal) {
+                    if (v.includes("-")) {
+                        const [a, b] = v.split("-").map(e => parseInt(e));
+                        if (id_num >= a && id_num <= b) {
+                            td.classList.add("nonmetal");
+                            break;
+                        }
+                    } else if (id_num === parseInt(v)) {
+                        td.classList.add("nonmetal");
+                        break;
+                    }
+                }
+            }
+            const textLen = electronsText[id_num - 1].length;
+            td.innerHTML = `<div class='id-num'>${id_num}</div><span class="elem-name">${elem}</span><div class='electron ${textLen > 3 ? "smaller" : textLen > 2 ? "small" : ""}'>${electronsText[id_num - 1].join("")}</div>`;
+            if (id_num === 160 || id_num === 280) id_num += 64 + 1;
+            else if (id_num === 396) id_num = 160 + 1;
+            else if (id_num === 160 + 64) id_num = 280 + 1;
+            else if (id_num < 396) id_num++;
+        }
+    }
+}
